e264b08572e93d1c837594abb44da1eaa15aa56f
Janak Mallik <janakmallik20@gmail.com>
2023-01-19 02:54:02 +0600

    up 2

---
+++ EbookCafeProject/Data/EbookCafeProjectContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EbookCafeProject.Data
+{
+    public class EbookCafeProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public EbookCafeProjectContext() : base("name=EbookCafeProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<EbookCafeProject.Models.User> Users { get; set; }
+    }
+}
---
+++ EbookCafeProject/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EbookCafeProject.Models
+{
+    public class User
+    {
+        public string userId { get; set; }  
+        public string userName { get; set; }
+        public string userEmail { get; set; }   
+        public string userPassword { get; set; }
+        public string userType { get; set; }
+
+        //public DateTime userDOB { get; set; }
+        //public string EmailConfirmed { get; set; }
+        //public string PasswordConfirmed { get; set; }
+
+    }
+}
